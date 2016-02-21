396dfd666b8d6bf91d8e82993a7714ab061a2e9d
LordMegaDeath3KHaxxor <marcus.t.lilja@gmail.com>
2016-02-21 22:44:28 +0100

    Camera follow.

---
+++ Endless Runner/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    private Transform player;
+
+    private Vector3 lastPlayerPosition;
+    private float distanceToMove;
+
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<PlayerController>().transform;
+        lastPlayerPosition = player.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        distanceToMove = player.position.x - lastPlayerPosition.x;
+        this.transform.position = new Vector3(transform.position.x + distanceToMove, transform.position.y, transform.position.z);
+
+        lastPlayerPosition = player.position;
+	}
+}
