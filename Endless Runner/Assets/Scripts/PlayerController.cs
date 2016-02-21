544d96c7ad79ea9f0bea8d92ad09804edfc11db6
LordMegaDeath3KHaxxor <marcus.t.lilja@gmail.com>
2016-02-21 22:34:34 +0100

    Basic player controller and animations.

---
+++ Endless Runner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed, jumpForce;
+
+    private Rigidbody2D rb2d;
+    private Animator animator;
+    private float moveVelocity;
+
+    private bool isGrounded = false;
+    public LayerMask groundMask;
+    private Collider2D playerCollider;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        playerCollider = GetComponent<Collider2D>();
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        isGrounded = Physics2D.IsTouchingLayers(playerCollider, groundMask);
+
+        moveVelocity = moveSpeed * Input.GetAxisRaw("Horizontal");
+        rb2d.velocity = new Vector2(moveVelocity, rb2d.velocity.y);
+
+        rb2d.velocity = new Vector2(moveVelocity, rb2d.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+        }
+
+        animator.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
+        animator.SetBool("Grounded", isGrounded); 
+	}
+}
