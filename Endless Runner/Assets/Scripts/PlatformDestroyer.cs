0ab6483da9b66205c4d3ad9431c7505ce07bfd4b
LordMegaDeath3KHaxxor <marli166@student.liu.se>
2016-02-27 17:34:44 +0100

    Spawned new platforms.

---
+++ Endless Runner/Assets/Scripts/PlatformDestroyer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformDestroyer : MonoBehaviour {
+
+    private Transform destroyPoint;
+
+	// Use this for initialization
+	void Start () {
+        destroyPoint = GameObject.FindGameObjectWithTag("DestroyPoint").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x < destroyPoint.position.x)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
